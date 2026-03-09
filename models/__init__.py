15f9efd7f324e2b921aaf328d1f03812f999b643
brendanwood29 <brendanwwood22@gmail.com>
2026-03-09 11:16:42 -0400

    [feat] added basic training functions

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,16 @@
 from .mlp import MLP
 
-__all__ = ['MLP']
+def get_model(name: str, **kwargs):
+        
+    configured_models = [
+        'mlp',
+    ]
+    
+    if name not in configured_models:
+        raise NotImplementedError(f'{name} is not a configured model, `name` must be one of {configured_models}')
+    
+    if name == 'mlp':
+        return MLP(
+            **kwargs
+        )
+        
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,14 @@
 from torch.utils.data import DataLoader
 from .dataset import BrainFuncDataset
-from  pathlib import Path
+from .loss_functions import get_loss_fn
+from .optimizers import get_optim
+from .schedulers import get_scheduler
+from pathlib import Path
 
-def get_train_loader(train_data_json: str | Path, step: int, batch_size=64, shuffle=True, **kwargs):
+def get_loader(data_json: str | Path, step: int, batch_size=64, shuffle=True, **kwargs):
     
     return DataLoader(
-        BrainFuncDataset(train_data_json, step),
+        BrainFuncDataset(data_json, step),
         batch_size=batch_size,
         shuffle=shuffle,
         **kwargs
