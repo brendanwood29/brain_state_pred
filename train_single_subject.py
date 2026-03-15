8f17f2bb55485c7ae77a64b433607cf703f69816
brendanwood29 <brendanwwood22@gmail.com>
2026-03-15 10:43:04 -0400

    [feat] added script to train a model one subject at a time

---
+++ train_single_subject.py
@@ -0,0 +1,88 @@
+import sys
+import torch
+from torch.utils.data import DataLoader
+from utils import Trainer, SingleSubjectBrainFuncDataset, split_single_subject
+from omegaconf.dictconfig import DictConfig
+from omegaconf.listconfig import ListConfig
+from omegaconf import OmegaConf
+from tqdm import tqdm
+from pathlib import Path
+
+def get_config() -> ListConfig | DictConfig:
+    
+    default_config_path = "configs/default_config.yaml"
+
+    cfg = OmegaConf.load(default_config_path)
+
+    if len(sys.argv) > 1:
+        user_cfg = OmegaConf.load(sys.argv[1])
+        cfg = OmegaConf.merge(cfg, user_cfg)
+
+    return cfg
+
+class SingleSubjectBrainStateTrainer(Trainer):
+    
+    def __init__(self, cfg):
+        super().__init__(cfg)
+    
+    def model_forward(self, batch):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = self.loss_fn(y_hat, y)
+        return loss
+
+
+if __name__ == '__main__':
+    
+    cfg = get_config()
+    
+
+    input_csv_list = list(Path('data_like-npi/hcp').rglob('**/*timeseries.csv'))
+    
+    for subject in tqdm(input_csv_list):
+        cfg.run_name = subject.name.removesuffix('_cleaned-timeseries.csv')
+        trainer = SingleSubjectBrainStateTrainer(cfg)
+        
+        train_data, test_data = split_single_subject(subject, cfg.data.train_proportion)
+        
+        train_loader = DataLoader(
+            SingleSubjectBrainFuncDataset(
+                train_data, 
+                cfg.data.train.step, 
+                cfg.device
+            ),
+            batch_size=cfg.batch_size,
+            shuffle=True
+        )
+            
+        test_loader = DataLoader(
+            SingleSubjectBrainFuncDataset(
+                test_data, 
+                cfg.data.test.step, 
+                cfg.device
+            ),
+            batch_size=cfg.batch_size,
+            shuffle=False
+        )
+        
+        
+        with tqdm(range(cfg.num_epochs)) as pbar:
+            for final_model_epochs in pbar:
+                trainer.train(train_loader)
+                pbar.set_postfix(
+                    {
+                        "train_loss": f"{trainer.loss_epoch[-1]:.4f}", 
+                        "val_loss": f"{trainer.last_val_loss:.4f}"
+                    }, 
+                    refresh=False
+                )
+                with torch.no_grad():
+                    should_stop = trainer.val(test_loader)
+                if should_stop:
+                    print(f'Stopped after {final_model_epochs} epochs due to early stopping.')
+                    break        
+        trainer.training_summary(final_model_epochs)
+            
+            
+            
+            
