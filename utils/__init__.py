b6e2060303a3144681d8283b06662a1dc5b3da87
brendanwood29 <brendanwwood22@gmail.com>
2026-03-17 20:37:36 -0400

    [fix] reverted to old version of the function

--- utils/__init__.py
+++ utils/__init__.py
@@ -8,21 +8,13 @@ from .make_datasplits import split_single_subject
 from pathlib import Path
 
 
-def get_loader(loader_type: str, data_path: str | Path, step: int, device: str, batch_size=64, shuffle=True, **kwargs):
+def get_loader(data_path: str | Path, step: int, device: str, batch_size=64, shuffle=True, **kwargs):
     
-    configured_loaders = [
-        'all'
-    ]
+
+    return DataLoader(
+        BrainFuncDataset(data_path, step, device),
+        batch_size=batch_size,
+        shuffle=shuffle,
+        **kwargs
+    )
     
-    if loader_type not in configured_loaders:
-        raise NotImplementedError(f'{loader_type} is not a configured scheduler, `name` must be one of {configured_loaders}')
-    
-    if loader_type == 'all':
-    
-        return DataLoader(
-            BrainFuncDataset(data_path, step, device),
-            batch_size=batch_size,
-            shuffle=shuffle,
-            **kwargs
-        )
-        
