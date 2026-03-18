608d237ae8abeff4e26236ba12bb03f2b299df0b
brendanwood29 <brendanwwood22@gmail.com>
2026-03-18 09:40:44 -0400

    [feat] initial transformer based model

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,12 @@
 from .mlp import MLP, ANN_MLP
+from .transformer_based import TransformerModel
 
 def get_model(name: str, **kwargs):
         
     configured_models = [
         'mlp',
-        'npi_mlp'
+        'npi_mlp',
+        'transformer'
     ]
     
     if name not in configured_models:
@@ -16,6 +18,10 @@ def get_model(name: str, **kwargs):
         )
     elif name == 'npi_mlp':
         return ANN_MLP(
+            **kwargs
+        )
+    elif name == 'transformer':
+        return TransformerModel(
             **kwargs
         )
         
---
+++ models/transformer_based.py
@@ -0,0 +1,235 @@
+import torch
+import torch.nn as nn
+
+class RotaryPositionalEmbedding(nn.Module):
+    """Rotary positional embedding, from google TimesFM, only for Q and K matrices"""
+
+    def __init__(
+        self,
+        embedding_dims: int,
+        min_timescale: float = 1.0,
+        max_timescale: float = 10000.0,
+    ):
+        super().__init__()
+        self.embedding_dims = embedding_dims
+        self.min_timescale = min_timescale
+        self.max_timescale = max_timescale
+
+    def forward(
+        self,
+        inputs: torch.Tensor,
+        position: torch.Tensor | None = None,
+    ):
+        """Generates a JTensor of sinusoids with different frequencies."""
+        if self.embedding_dims != inputs.shape[-1]:
+            raise ValueError(
+                "The embedding dims of the rotary position embedding"
+                "must match the hidden dimension of the inputs."
+            )
+        half_embedding_dim = self.embedding_dims // 2
+        fraction = (
+            2
+            * torch.arange(0, half_embedding_dim, device=inputs.device)
+            / self.embedding_dims
+        )
+        timescale = (
+            self.min_timescale * (self.max_timescale / self.min_timescale) ** fraction
+        ).to(inputs.device)
+        if position is None:
+            seq_length = inputs.shape[1]
+            position = torch.arange(
+                seq_length, dtype=torch.float32, device=inputs.device
+            )[None, :]
+
+        if len(inputs.shape) == 4:
+            position = position[..., None, None]
+            timescale = timescale[None, None, None, :]
+        elif len(inputs.shape) == 3:
+            position = position[..., None]
+            timescale = timescale[None, None, :]
+        else:
+            raise ValueError("Inputs must be of rank 3 or 4.")
+
+        sinusoid_inp = position / timescale
+        sin = torch.sin(sinusoid_inp)
+        cos = torch.cos(sinusoid_inp)
+        first_half, second_half = torch.chunk(inputs, 2, dim=-1)
+        first_part = first_half * cos - second_half * sin
+        second_part = second_half * cos + first_half * sin
+        return torch.cat([first_part, second_part], dim=-1)
+
+
+class MultiHeadSelfAttention(nn.Module):
+    
+    def __init__(
+        self,
+        in_features,
+        num_heads,
+        steps: int = 3,
+        attn_drop_prob: float = 0.1,
+        final_drop_prob: float = 0.1
+    ):
+        super().__init__()
+        
+        self.num_heads = num_heads
+        self.factor = ((in_features // self.num_heads) ** -0.5)
+        self.qkv_proj = nn.Linear(in_features, 3 * in_features, bias=True)
+        self.final_proj = nn.Linear(in_features, in_features, bias=True)
+        self.attn_drop = nn.Dropout(attn_drop_prob)
+        self.final_drop = nn.Dropout(final_drop_prob)
+        self.pos_embed = RotaryPositionalEmbedding(in_features // num_heads, 1, steps)
+        
+        
+        self.register_buffer(
+            'mask',
+            torch.triu(torch.ones(steps, steps), diagonal=1).bool()
+        )
+        
+        
+    def forward(self, x):
+        
+        B, N, C = x.shape
+        qkv = self.qkv_proj(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        q = self.pos_embed(q)
+        k = self.pos_embed(k)
+        
+        attn = q @ k.transpose(-2, -1)
+        attn *= self.factor
+        attn = attn.masked_fill(self.mask, -torch.inf)
+        attn = attn.softmax(dim=-1)
+        attn_drop = self.attn_drop(attn)
+        
+        out = (attn_drop @ v).transpose(1, 2).reshape(B, N, C)
+        out = self.final_proj(out)
+        out = self.final_drop(out)
+        
+        return out
+
+
+class MultiHeadCrossAttention(nn.Module):
+    
+    def __init__(
+        self,
+        in_features,
+        num_heads,
+        final_drop_prob: float = 0.1
+    ):
+        super().__init__()
+        
+        self.num_heads = num_heads
+        self.factor = ((in_features // self.num_heads) ** -0.5)
+        self.q_proj = nn.Linear(in_features, in_features, bias=True)
+        self.kv_proj = nn.Linear(in_features, 2 * in_features, bias=True)
+        self.final_proj = nn.Linear(in_features, in_features, bias=True)
+        self.final_drop = nn.Dropout(final_drop_prob)
+        
+    
+    def forward(self, x):
+        
+        B, N, C = x.shape
+        q, kv = x[:, 0, :], x[:, 1:, :]
+        q = self.q_proj(q).reshape(
+            B, 
+            1, 
+            self.num_heads, 
+            C // self.num_heads
+        ).permute(0, 2, 1, 3)
+        kv = self.kv_proj(kv).reshape(
+            B, 
+            N - 1, 
+            2, 
+            self.num_heads, 
+            C // self.num_heads
+        ).permute(2, 0, 3, 1, 4) # kv-dim, B, N, Num heads, head dim
+        
+        k, v = kv[0], kv[1]
+        attn = q @ k.transpose(-2, -1)
+        attn *= self.factor
+        attn = attn.softmax(dim=-1)
+        attn_drop = nn.Identity()(attn)
+        
+        out = (attn_drop @ v).transpose(1, 2).reshape(B, 1, C)
+        out = self.final_drop(out)
+        out = self.final_drop(out)
+
+
+class FFN(nn.Module):
+    
+    def __init__(
+        self,
+        in_features: int,
+    ):
+        super().__init__()
+        
+        self.layer1 = nn.Linear(in_features, in_features)
+        self.layer2 = nn.Linear(in_features, in_features)
+        self.activation = nn.GELU()
+    
+    def forward(self, x):
+        
+        x = self.layer1(x)
+        x = self.activation(x)
+        x = self.layer2(x)
+        
+        return x
+
+
+class Block(nn.Module):
+    
+    def __init__(
+        self,
+        in_features: int,
+        num_heads: int,
+        steps: int
+    ):
+        super().__init__()
+        self.attn = MultiHeadSelfAttention(in_features, num_heads, steps)
+        self.ffn = FFN(in_features)
+        self.layer_norm1 = nn.LayerNorm((steps, in_features))
+        self.layer_norm2 = nn.LayerNorm((steps, in_features))
+    
+    def forward(self, x):
+        """
+            x must have shape (Batch, Num timepoints, Num Regions)
+        """
+        
+        x = self.layer_norm1(x)
+        x = x + self.attn(x)
+        x = self.layer_norm2(x)
+        x = x + self.ffn(x)
+        
+        return x
+
+
+
+class TransformerModel(nn.Module):
+    
+    def __init__(
+        self,
+        num_blocks: int,
+        in_features: int,
+        num_heads: int,
+        steps: int,
+    ) -> None:
+        super().__init__()
+        
+        self.mod_list = [
+            Block(in_features, num_heads, steps) for _ in range(num_blocks)
+        ]
+        
+        self.model = nn.Sequential(*self.mod_list)
+        
+    def forward(self, x):
+        """
+            x must have shape (Batch, Num timepoints, Num Regions)
+        """
+        
+        x = self.model(x)
+        
+        return x[:, -1, :]
+    
+if __name__ == "__main__":
+    
+    
+    pass
