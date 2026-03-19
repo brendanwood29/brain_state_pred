68763a8303c0234eb6101965d46e3e1309293b26
brendanwood29 <brendanwwood22@gmail.com>
2026-03-19 14:37:20 +0000

    [feat] added graph based model (GCN STGCN)

--- models/graph_based.py
+++ models/graph_based.py
@@ -1,6 +1,7 @@
 import torch_geometric.nn as gnn
 from torch import nn
 from typing import Callable
+from torch_geometric_temporal.nn.attention import STConv
 
 
 class GCN(nn.Module):
@@ -9,21 +10,44 @@ class GCN(nn.Module):
         self, 
         num_features: int, 
         hidden_dim: int,
-        activation: Callable = nn.ReLU
+        activation: Callable = nn.GELU
     ):
         super().__init__()
         self.conv_in = gnn.GCNConv(num_features, hidden_dim)
-        self.conv_mid = gnn.GCNConv(hidden_dim, hidden_dim)
+        self.conv_mid_1 = gnn.GCNConv(hidden_dim, hidden_dim)
+        self.conv_mid_2 = gnn.GCNConv(hidden_dim, hidden_dim)
         self.conv_final = gnn.GCNConv(hidden_dim, 1)
         self.activation = activation()
 
     def forward(self, x, edge_index, edge_weight):
         x = self.conv_in(x, edge_index, edge_weight)
         x = self.activation(x)
-        x = self.conv_mid(x, edge_index, edge_weight)
+        x = self.conv_mid_1(x, edge_index, edge_weight)
         x = self.activation(x)
-        x = self.conv_mid(x, edge_index, edge_weight)
+        x = self.conv_mid_2(x, edge_index, edge_weight)
         x = self.activation(x)
         x = self.conv_final(x, edge_index, edge_weight)
 
         return x
+    
+class STGCN(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.rec_1 = STConv(360, 1, 12, 1, 3, 2)
+        self.rec_2 = STConv(360, 1, 12, 1, 3, 2)
+        self.lin = nn.Linear(360, 360)
+        self.activate = nn.ReLU()
+        
+    def forward(self, x, edge_index, edge_weight=None):
+        x = self.rec_1(x, edge_index, edge_weight)
+        x = self.activate(x)
+        x = self.rec_2(x, edge_index, edge_weight)
+        x = self.activate(x)
+        x = self.lin(x[:, -1, ...].squeeze(-1))
+        return x
+
+
+    
+    
