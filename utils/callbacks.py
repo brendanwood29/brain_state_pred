69725469d3578bf58726408ea7c995d4e222b67c
brendanwood29 <brendanwwood22@gmail.com>
2026-03-25 17:38:18 +0000

    [feat] added threshold to check if difference is big enough

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,16 +1,19 @@
 class EarlyStopping():
 
-  def __init__(self, patience: int, use: bool = True) -> None:
+  def __init__(self, patience: int, use: bool = True, threshold: float 0.001) -> None:
     self.patience = patience
     self.best_loss = None
     self.not_better_count = 0
     self.use = use
+    self.threshold = threshold
 
   def __call__(self, val_loss: float):
     if self.best_loss is None:
       self.best_loss = val_loss
       return False
-    elif val_loss < self.best_loss:
+    # elif val_loss < self.best_loss:
+    elif (self.best_loss - val_loss) > self.threshold: 
+      self.best_loss = val_loss
       self.not_better_count = 0
       return False
     else:
@@ -18,4 +21,4 @@ class EarlyStopping():
       if self.not_better_count >= self.patience:
         return True
       else:
-        return False    
+        return False
