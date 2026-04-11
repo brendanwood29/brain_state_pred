0ce7d78088ca9e5fd07e0f0b4546eb8c62f79f01
brendanwood29 <brendanwwood22@gmail.com>
2026-04-11 17:25:39 +0000

    [feat] changed to follow new style

--- models/__init__.py
+++ models/__init__.py
@@ -1,49 +1,18 @@
 from .mlp import MLP, ANN_MLP
 from .lstm import LSTM
 from .transformer_based import TransformerModel
-from .graph_based import GCN, STGCN, GCTN
+from .graph_based import GCN, STGCN
+from pytorch_trainer import ModelGetter
 
-def get_model(name: str, **kwargs):
-        
-    configured_models = [
-        'mlp',
-        'npi_mlp',
-        'transformer',
-        'gcn',
-        'stgcn',
-        'lstm',
-        'gctn'
-    ]
-    
-    if name not in configured_models:
-        raise NotImplementedError(f'{name} is not a configured model, `name` must be one of {configured_models}')
-    
-    if name == 'mlp':
-        return MLP(
-            **kwargs
-        )
-    elif name == 'npi_mlp':
-        return ANN_MLP(
-            **kwargs
-        )
-    elif name == 'transformer':
-        return TransformerModel(
-            **kwargs
-        )
-    elif name == 'gcn':
-        return GCN(
-            **kwargs
-        )
-    elif name == 'stgcn':
-        return STGCN(
-            **kwargs
-        )
-    elif name == 'lstm':
-        return LSTM(
-            **kwargs
-        )
-    elif name == 'gctn':
-        return GCTN(
-            **kwargs
-        )
-        
+
+npi_model_getter = ModelGetter(
+    {
+        'mlp': MLP,
+        'npi_mlp':ANN_MLP,
+        'transformer': TransformerModel,
+        'gcn': GCN,
+        'stgcn': STGCN,
+        'lstm': LSTM
+    }
+)
+
